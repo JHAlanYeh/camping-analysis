4597a7bf78012ae6afc8fe17b16bd1aeeca6c658
Alan Yeh <huyij486491@gmail.com>
2024-02-04 01:12:01 +0800

    add easycamp

--- webcrawling_camping.py
+++ webcrawling_camping.py
@@ -1,57 +1,105 @@
 import requests
 import json
-import math
 import os
+from bs4 import BeautifulSoup
 
-dir_path = os.path.join(os.getcwd(), "data")
-json_list = os.listdir(dir_path)
 
-max_seq = 0
-for file in json_list:
-    if ".json" not in file:
-        continue
-    file_slice = file.split("_")
-    file_seq = int(file_slice[1].replace(".json", ""))
-    if file_seq > max_seq:
-        max_seq = file_seq
+def asiayo_crawler():
+    print("==================== Start Asiayo Crawler ====================")
+    dir_path = os.path.join(os.getcwd(), "data")
+    camping_area_url = "https://asiayo.com/zh-tw/view/tw/{city_code}/{code}/?tags=camping&isFromPropertyPage=true/"
+    offset = 0
 
-dir_path = os.path.join(os.getcwd(), "data")
-camping_area_url = "https://asiayo.com/zh-tw/view/tw/{city_code}/{code}/?tags=camping&isFromPropertyPage=true/"
-offset = 0
-camping_area_objs = []
-while True:
-    res = requests.get("""https://web-api.asiayo.com/api/v1/bnbs/search?locale=zh-tw&currency=TWD&checkInDate=2024-02-24&checkOutDate=2024-02-25&city=nantou-county&adult=4&quantity=1&type=country&country=tw&tags=camping&offset={offset}""".format(offset=offset))
-    if res.status_code == 200:
-        res_json = res.json()
-        camping_areas = res_json["data"]["rows"]
-        if len(camping_areas) == 0:
-            break
-        
-        for area in camping_areas:
-            name = area["name"]
-            city = area["address"]["city"]["name"]
-            district = area["address"]["area"]["name"]
-            code = area["id"]
-            city_code = area["address"]["city"]["urlName"]
-            # 經度
-            latitude = area["location"]["lat"]
-            # 緯度
-            longitude = area["location"]["lng"]
-            url = camping_area_url.format(city_code=city_code, code=code)
+    file_name = os.path.join(dir_path, 'camping_asiayo.json')
+    f = open(file_name, encoding="utf-8-sig")
+    camping_area_objs = json.load(f)
+    new_obj = 0
+    while True:
+        res = requests.get("""https://web-api.asiayo.com/api/v1/bnbs/search?locale=zh-tw&currency=TWD&checkInDate=2024-03-21&checkOutDate=2024-03-22&adult=4&quantity=1&type=country&country=tw&tags=camping&offset={offset}""".format(offset=offset))
+        if res.status_code == 200:
+            res_json = res.json()
+            camping_areas = res_json["data"]["rows"]
+            if len(camping_areas) == 0:
+                break
+            
+            for area in camping_areas:
+                code = area["id"]
+                if next((item for item in camping_area_objs if item["code"] == code), None) is not None:
+                    continue
+                name = area["name"]
+                city = area["address"]["city"]["name"]
+                district = area["address"]["area"]["name"]
+                city_code = area["address"]["city"]["urlName"]
+                # 經度
+                latitude = area["location"]["lat"]
+                # 緯度
+                longitude = area["location"]["lng"]
+                url = camping_area_url.format(city_code=city_code, code=code)
 
+                camping_area_objs.append({
+                    "code": code,
+                    "name": name,
+                    "city": city,
+                    "district": district,
+                    "region":city + district,
+                    "url": url,
+                    "latitude":latitude,
+                    "longitude":longitude
+                })
+                new_obj += 1
+        offset += 20
+            
+
+    print("New:{}".format(str(new_obj)))
+    print("Total:{}".format(len(camping_area_objs)))
+    sorted_camping_objs = sorted(camping_area_objs, key=lambda d: d["code"]) 
+    with open(file_name, 'w', encoding="utf-8-sig") as f:
+        json.dump(sorted_camping_objs, f, indent=4, ensure_ascii=False)
+        print("save {file_name}".format(file_name=file_name))
+
+    print("==================== End Asiayo Crawler ====================")
+
+
+def easycamp_crawler():
+    print("==================== Start EasyCamp Crawler ====================")
+
+    dir_path = os.path.join(os.getcwd(), "data")
+    base_url = "https://www.easycamp.com.tw{relative_link}"
+
+    file_name = os.path.join(dir_path, 'camping_easycamp.json')
+    camping_area_objs = []
+
+    main_url = "https://www.easycamp.com.tw/AC_Stores.html"
+    web = requests.get(main_url)
+    soup = BeautifulSoup(web.text, "html.parser")
+    all_country = soup.select("[id^='store_menu_']")
+    for country in all_country:
+        link =  base_url.format(relative_link=country.select_one("li a").get('href'))
+        camp_region = requests.get(link)
+        soup = BeautifulSoup(camp_region.text, "html.parser")
+        articles = soup.select('article')
+        for article in articles:
+            code = article.select_one('a').get('href').replace("/Store_", "").replace(".html", "")
+            url = base_url.format(relative_link=article.select_one('a').get('href'))
+            name = article.select_one('h3 a').text
+            region = article.select_one('.companyinfo a').text
             camping_area_objs.append({
                 "code": code,
                 "name": name,
-                "city": city,
-                "district": district,
+                "region":region,
                 "url": url,
-                "latitude":latitude,
-                "longitude":longitude
             })
-    offset += 20
-        
-print("Total:{}".format(str(len(camping_area_objs))))
-file_name = os.path.join(dir_path, 'camping_{}.json'.format(str(max_seq+1)))
-with open(file_name, 'w', encoding="utf-8-sig") as f:
-    json.dump(camping_area_objs, f, indent=4, ensure_ascii=False)
-    print("save {file_name}".format(file_name=file_name))
+
+
+    print("Total:{}".format(len(camping_area_objs)))
+    sorted_camping_objs = sorted(camping_area_objs, key=lambda d: d["url"]) 
+    with open(file_name, 'w', encoding="utf-8-sig") as f:
+        json.dump(sorted_camping_objs, f, indent=4, ensure_ascii=False)
+        print("save {file_name}".format(file_name=file_name))
+
+    print("==================== End EasyCamp Crawler ====================")
+
+
+if __name__ == "__main__":
+    # asiayo_crawler()
+    easycamp_crawler()
--- webcrawling_comment.py
+++ webcrawling_comment.py
@@ -1,34 +1,34 @@
 import requests
 import json
-import math
 import os
-from pathlib import Path
+from bs4 import BeautifulSoup
 
-dir_path = os.path.join(os.getcwd(), "data")
-json_list = os.listdir(dir_path)
+def asiayo_comment_crawler():
+    dir_path = os.path.join(os.getcwd(), "data")
+    file = "camping_asiayo.json"
+    total_comment_count = 0
 
-for file in json_list:
-    if ".json" not in file or "_finish" in file:
-        continue
     file_path = os.path.join(dir_path, file)
     f = open(file_path, encoding="utf-8-sig")
     data = json.load(f)
+    f.close()
     for d in data:
         offset = 0
         code = d["code"]
 
-        save_dir = os.path.join(dir_path, "comments")
+        save_dir = os.path.join(dir_path, "asiayo_comments")
         os.makedirs(save_dir, exist_ok=True)
         file_name = os.path.join(dir_path, '{save_dir}\\comment_{code}.json'.format(save_dir=save_dir, code=code))
 
-        if os.path.exists(file_name):
-            print("file existed")
-            continue
+        # if os.path.exists(file_name):
+        #     print("file existed")
+        #     continue
 
         comment_objs = []
         res = requests.get("https://web-api.asiayo.com/api/v1/bnbs/{code}?locale=zh-tw&currency=TWD&checkInDate=2024-02-03&checkOutDate=2024-02-04&people=1&adult=1&childAges=".format(code=code))
         res_json = res.json()
         d["address"] = res_json["data"]["address"]["fullAddress"]
+        d["description"] = res_json["data"]["description"]
 
         while True:
             res = requests.get("""https://web-api.asiayo.com/api/v1/bnbs/{code}/reviews?limit=10&offset={offset}&locale=zh-tw""".format(code=code, offset=offset))
@@ -55,9 +55,68 @@ for file in json_list:
         with open(file_name, 'w', encoding="utf-8-sig") as f:
             json.dump(d, f, indent=4, ensure_ascii=False)
             print("save {file_name}".format(file_name=file_name))
-    
 
+        total_comment_count += len(comment_objs)
+        print("Total Comment Count：{}".format(total_comment_count))
+
+
+def easycamp_comment_crawler():
+    dir_path = os.path.join(os.getcwd(), "data")
+    file = "camping_easycamp.json"
+    total_comment_count = 0
+    comment_url = "https://www.easycamp.com.tw/store/purchase_rank/{code}/4/{page}"
+
+    file_path = os.path.join(dir_path, file)
+    f = open(file_path, encoding="utf-8-sig")
+    data = json.load(f)
     f.close()
-    print("{} has finished".format(file))
-    p = Path(file_path)
-    p.rename(Path(p.parent, f"{p.stem}_finish{p.suffix}"))
+
+    for d in data:
+        code = d["code"]
+        save_dir = os.path.join(dir_path, "easycamp_comments")
+        os.makedirs(save_dir, exist_ok=True)
+        file_name = os.path.join(dir_path, '{save_dir}\\comment_{code}.json'.format(save_dir=save_dir, code=code))
+        comment_objs = []
+        web = requests.get(d["url"])
+        soup = BeautifulSoup(web.text, "html.parser")
+        d["description"] = soup.select_one("#content_id").text.strip()
+        d["address"] = soup.select_one(".camp-info .camp-add").text.strip()
+        gps = soup.select_one(".camp-info div .camp-gps").text.strip()
+        if gps != "":
+            d["latitude"] = float(soup.select_one(".camp-info div .camp-gps").text.strip().split(',')[0])
+            d["longitude"] = float(soup.select_one(".camp-info div .camp-gps").text.strip().split(',')[1])
+        page = 1
+        while True:
+            camp_comment = requests.get(comment_url.format(code=code, page=page))
+            soup = BeautifulSoup(camp_comment.text, "html.parser")
+            comments = soup.select('#tab11 div .row')
+            if len(comments) == 0:
+                break
+            for comment in comments:
+                publishedDate = comment.select_one("div .evaluation-font-color div:nth-child(3)").text.replace("評價：", "").strip()
+                contents = comment.select(".english-break-word")
+                content = []
+                if contents[0].text.strip() != "":
+                    content.append(contents[0].text.strip())
+                if contents[1].text.strip() != "":
+                    content.append(contents[1].text.strip())
+
+                rating = len(comment.select(".fa.fa-star"))
+                comment_objs.append({
+                    "content": "。".join(content),
+                    "rating": rating,
+                    "publishedDate": publishedDate
+                })
+            page += 1
+
+        d["comments"] = comment_objs
+        with open(file_name, 'w', encoding="utf-8-sig") as f:
+            json.dump(d, f, indent=4, ensure_ascii=False)
+            print("save {file_name}".format(file_name=file_name))
+
+        total_comment_count += len(comment_objs)
+        print("Total Comment Count：{}".format(total_comment_count))
+
+if __name__ == "__main__":
+    # asiayo_comment_crawler()
+    easycamp_comment_crawler()
