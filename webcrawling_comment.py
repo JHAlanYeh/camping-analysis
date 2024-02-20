d3b907cea24e82a5630190745d6c4f8a1d653814
Alan Yeh <huyij486491@gmail.com>
2024-02-20 19:06:07 +0800

    no message

--- webcrawling_comment.py
+++ webcrawling_comment.py
@@ -335,10 +335,10 @@ def klook_comment_crawler():
     'Accept-Encoding': "gzip, deflate",
     'Connection': "keep-alive",
     'cache-control': "no-cache",
-    'Cookie': 'klk_currency=TWD; kepler_id=761ce78c-e8c4-4ed1-927d-fa3543f67eec; persisted_source=viviantrip.tw; k_tff_ch=aid_2620; _gcl_au=1.1.1239040881.1707752204; __lt__cid=e84c815c-4379-4377-9952-147530418dc4; __lt__cid.c83939be=e84c815c-4379-4377-9952-147530418dc4; _fwb=237XOdedFIxX1kiZGCqzRJx.1707752212523; _yjsu_yjad=1707752215.d311e5f3-cbae-44d7-8508-215e64247bbe; _tt_enable_cookie=1; _ttp=j6D-oFE6A5T30gS1d0a58mwBUT4; dable_uid=3221839.1700147487968; KOUNT_SESSION_ID=5CDFC80C89656DD1034CD99E124D4D63; clientside-cookie=737969e068a2f6407ca720bbfdc275feccaa31d9fef47c0a772d2e9e6048c024f2771ca16c3cbee8bbd936f8e66ee7b3e47a168fad8bd99079cb74cdbdcaf263883e67eec02f3fcd7e67a327d8edeaf3b9b3a0dadf3f9d66db4fc612f2a7a7e4c76c62b3ba8efdcc84e964afe34e405f3eb110689ef08761e04765cca78e1ae74b238a28fc71c058c6b783e6c7b4dc891228d5367b6db972032610; aid=2620; wid=2620; aid_query_string=aid%3D2620; affiliate_type=non-network; aid_extra=%7B%22aff_klick_id%22%3A%2259183430999-2620-0-4d4002f%22%2C%22affiliate_partner%22%3A%22%22%2C%22content%22%3A%22%22%7D; aid_campaign=aid=2620&utm_medium=affiliate-alwayson&utm_source=non-network&utm_campaign=2620; tr_update_tt=1707925831001; campaign_tag=klc_l1=Affiliate; traffic_retain=true; klk_ps=1; TNAHD=c42_1707926053329__c8109_1708008346057__c27456_1707926044903__c6488_1707925903585; klk_rdc=TW; __lt__sid=85b1645f-384bd282; __lt__sid.c83939be=85b1645f-384bd282; _gid=GA1.2.593634834.1708246975; ftr_blst_1h=1708246976515; _ga_TH9DNLM4ST=GS1.1.1708246976.9.1.1708248045.60.0.0; klk_ga_sn=8784941859..1708248046323; _dc_gtm_UA-86696233-1=1; wcs_bt=s_2cb388a4aa34:1708248047; _uetsid=80b5c1b0ce3c11eebd2045203b2b2150; _uetvid=d63970c0d13a11ed9dbf65988fc79cf5; _ga=GA1.1.1057024504.1574164938; _ga_FW3CMDM313=GS1.1.1708246974.9.1.1708248047.0.0.0; _ga_V8S4KC8ZXR=GS1.1.1708246975.9.1.1708248048.57.0.0; forterToken=1f3f45b14ff64e5b9e7ab1e8ff24de94_1708248048757__UDF43-m4_20ck; klk_i_sn=9724350595..1708248048805; datadome=1~__UB9JVuiydl2nSjSIvfXWHwZDgMoV2dBRhm0CCrDFtABU5093VQ5xr8W0gptOIVTgb6baSAe6k~GMOvYHevubmBFXIAy6tAxfD_yxcXqgIszJnRkplUptfn55aY64'
+    'Cookie': 'klk_currency=TWD; kepler_id=761ce78c-e8c4-4ed1-927d-fa3543f67eec; persisted_source=viviantrip.tw; k_tff_ch=aid_2620; _gcl_au=1.1.1239040881.1707752204; __lt__cid=e84c815c-4379-4377-9952-147530418dc4; __lt__cid.c83939be=e84c815c-4379-4377-9952-147530418dc4; _fwb=237XOdedFIxX1kiZGCqzRJx.1707752212523; _yjsu_yjad=1707752215.d311e5f3-cbae-44d7-8508-215e64247bbe; _tt_enable_cookie=1; _ttp=j6D-oFE6A5T30gS1d0a58mwBUT4; dable_uid=3221839.1700147487968; KOUNT_SESSION_ID=5CDFC80C89656DD1034CD99E124D4D63; clientside-cookie=737969e068a2f6407ca720bbfdc275feccaa31d9fef47c0a772d2e9e6048c024f2771ca16c3cbee8bbd936f8e66ee7b3e47a168fad8bd99079cb74cdbdcaf263883e67eec02f3fcd7e67a327d8edeaf3b9b3a0dadf3f9d66db4fc612f2a7a7e4c76c62b3ba8efdcc84e964afe34e405f3eb110689ef08761e04765cca78e1ae74b238a28fc71c058c6b783e6c7b4dc891228d5367b6db972032610; aid=2620; wid=2620; aid_query_string=aid%3D2620; affiliate_type=non-network; aid_extra=%7B%22aff_klick_id%22%3A%2259183430999-2620-0-4d4002f%22%2C%22affiliate_partner%22%3A%22%22%2C%22content%22%3A%22%22%7D; aid_campaign=aid=2620&utm_medium=affiliate-alwayson&utm_source=non-network&utm_campaign=2620; tr_update_tt=1707925831001; campaign_tag=klc_l1=Affiliate; traffic_retain=true; klk_ps=1; TNAHD=c42_1707926053329__c8109_1708008346057__c27456_1707926044903__c6488_1707925903585; klk_rdc=TW; __lt__sid=85b1645f-bf08c2ed; __lt__sid.c83939be=85b1645f-bf08c2ed; klk_ga_sn=2497024496..1708352802287; wcs_bt=s_2cb388a4aa34:1708352802; _gid=GA1.2.1833952077.1708352803; _dc_gtm_UA-86696233-1=1; _uetsid=e719e660cf3211eeb300b17c6029affa; _uetvid=d63970c0d13a11ed9dbf65988fc79cf5; _ga_FW3CMDM313=GS1.1.1708352802.10.0.1708352802.0.0.0; _ga_V8S4KC8ZXR=GS1.1.1708352803.10.0.1708352803.60.0.0; klk_i_sn=1340718126..1708352803824; _ga_TH9DNLM4ST=GS1.1.1708352806.10.1.1708352806.60.0.0; datadome=wYzO3xY7M2TOta3kawiUgulI0xma8o0sUxd6dHyKoPsHKpXaM5EGKcIpj0bv4EQR4fUWsXvQhnqhVveJZroYgFoOZceaZpqShIez7mLRzQbFPLGqNhdAIRLmK5TmUGc6; _ga=GA1.2.1057024504.1574164938; forterToken=1f3f45b14ff64e5b9e7ab1e8ff24de94_1708352804108__UDF4_20ck'
     }
 
-    data = sorted(data, key=lambda x: x["code"], reverse=True)
+    # data = sorted(data, key=lambda x: x["code"], reverse=True)
 
     for d in data:
         if d["disabled"] == 1 or d["type"] == 4:
@@ -358,6 +358,7 @@ def klook_comment_crawler():
             querystring = {"k_lang":"zh_TW","k_currency":"TWD","activity_id":code,"page":page,"limit":"8","sort_type":"0","only_image": "false"}
             res = requests.get(""" https://www.klook.com/v1/experiencesrv/activity/component_service/activity_reviews_list""", headers=headers, params=querystring)
             print(res.status_code)
+            print(page)
             if res.status_code == 200:
                 res_json = res.json()
                 camping_comments = res_json["result"]["item"]
@@ -415,6 +416,14 @@ def klook_comment_crawler():
 def klook_comment_tokenization():
     dir_path = os.path.join(os.getcwd(), "data")
     klook_dir = os.path.join(dir_path, "klook_comments")
+    
+    total_type_1_count = 0
+    total_type_2_count = 0
+    total_type_3_count = 0
+
+    conform_type_1_count = 0
+    conform_type_2_count = 0
+    conform_type_3_count = 0
 
     for i in range(1, 4 + 1):
         klook_type_dir = os.path.join(klook_dir, str(i))
@@ -430,15 +439,42 @@ def klook_comment_tokenization():
                         if word not in STOP_WORDS:
                             new_ws.append(word)
                     c["tokenization"] = " | ".join(new_ws)
-
+                
                 with open(file_path, 'w', encoding="utf-8-sig") as nf:
                     json.dump(data, nf, indent=4, ensure_ascii=False)
                     print("save {file_name}".format(file_name=file_path))
 
+                if data["type"] == 1:
+                    total_type_1_count += len(data["comments"])
+                    conform_type_1_count += 1
+                elif data["type"] == 2:
+                    total_type_2_count += len(data["comments"])
+                    conform_type_2_count += 1
+                elif data["type"] == 3:
+                    total_type_3_count += len(data["comments"])
+                    conform_type_3_count += 1
+
+    print("Total Type 1 Count：{}".format(total_type_1_count))
+    print("Total Type 2 Count：{}".format(total_type_2_count))
+    print("Total Type 3 Count：{}".format(total_type_3_count))
+
+    save_dir = os.path.join(dir_path, "klook_comments")
+    file_name = os.path.join(save_dir, 'klook_info.json')
+    with open(file_name, 'w', encoding="utf-8-sig") as f:
+        overview = {
+            "conform_type_1": conform_type_1_count,
+            "conform_type_2": conform_type_2_count,
+            "conform_type_3": conform_type_3_count,
+            "type_1_comments":total_type_1_count,
+            "type_2_comments":total_type_2_count,
+            "type_3_comments":total_type_3_count,
+        }
+        json.dump(overview, f, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     # asiayo_comment_tokenization()
     # easycamp_comment_tokenization()
-    # klook_comment_tokenization()
-    asiayo_comment_crawler()
-    easycamp_comment_crawler()
+    klook_comment_tokenization()
+    # asiayo_comment_crawler()
+    # easycamp_comment_crawler()
     # klook_comment_crawler()
