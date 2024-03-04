4f8720a3eaaedbacf238121316328e192ddecca0
Alan Yeh <huyij486491@gmail.com>
2024-03-04 18:56:50 +0800

    no message

--- webcrawling_star.py
+++ webcrawling_star.py
@@ -69,6 +69,44 @@ def star_range_distributed():
         json.dump(star_distributed, f, indent=4, ensure_ascii=False)
 
 
+def google_star_range_distributed():
+    star_1_cnt = 0
+    star_2_cnt = 0
+    star_3_cnt = 0
+    star_4_cnt = 0
+    star_5_cnt = 0
+
+    
+    dir_path = os.path.join(os.getcwd(), "data\\google_comments")
+    for file in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file)
+        f = open(file_path, encoding="utf-8-sig")
+        data = json.load(f)
+        for c in data["comments"]:
+            if c["rating"] == 1:
+                star_1_cnt += 1
+            elif c["rating"] == 2:
+                star_2_cnt += 1
+            elif c["rating"] == 3:
+                star_3_cnt += 1
+            elif c["rating"] == 4:
+                star_4_cnt += 1
+            elif c["rating"] == 5:
+                star_5_cnt += 1
+
+        save_path = os.path.join(os.getcwd(), "data\\star_distributed")
+        file_name = os.path.join(save_path, 'type_google_star_.json')
+        with open(file_name, 'w', encoding="utf-8-sig") as f:
+            star_distributed = {
+                "star_1": star_1_cnt,
+                "star_2": star_2_cnt,
+                "star_3": star_3_cnt,
+                "star_4":star_4_cnt,
+                "star_5":star_5_cnt,
+            }
+            json.dump(star_distributed, f, indent=4, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     star_range_distributed()
+    google_star_range_distributed()
