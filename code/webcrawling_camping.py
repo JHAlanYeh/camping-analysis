af2784366253043f9249c8cec6b965faea099a68
DESKTOP-P4V0NDV\Alan <huyij486491@gmail.com>
2024-05-08 00:22:39 +0800

    no message

--- code/webcrawling_camping.py
+++ code/webcrawling_camping.py
@@ -204,19 +204,19 @@ def klook_crawler():
 
 def kkday_crawler():
     print("==================== Start KKday Crawler ====================")
-    url = "https://www.kkday.com/zh-tw/product/productlist?page={}&city=A01-001-00002%2CA01-001-00010%2CA01-001-00009%2CA01-001-00012%2CA01-001-00006%2CA01-001-00001%2CA01-001-00008%2CA01-001-00014%2CA01-001-00017%2CA01-001-00018%2CA01-001-00003%2CA01-001-00026%2CA01-001-00005%2CA01-001-00013%2CA01-001-00004%2CA01-001-00007%2CA01-001-00011%2CA01-001-00015%2CA01-001-00016&keyword=%E9%9C%B2%E7%87%9F&sort=prec"
+    url = "https://www.kkday.com/zh-tw/product/ajax_productlist"
     
     
-    browserOptions = webdriver.ChromeOptions()
-    browserOptions.add_argument("--start-maximized")
-
-    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15"
-    browserOptions.add_argument('--user-agent={}'.format(user_agent))
-    # browserOptions.add_argument("--headless")
-    browserOptions.add_argument('--mute-audio')
-    # INFO_0 / WARNING_1 / ERROR_2 / FATAL_3 / DEFAULT_0
-    browserOptions.add_argument("log-level=3")
-    # ****************************************************************************** #
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        'Accept': "application/json, text/plain, */*",
+        'Cache-Control': "no-cache",
+        'Accept-Language': "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6",
+        'Accept-Encoding': "gzip, deflate, br, zstd",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache",
+        'Cookie': 'country_lang=zh-tw; currency=TWD; KKUD=538952ab1d2b5c12cede14260c12d8dc; _gcl_au=1.1.1513969168.1714914403; __lt__cid=17633441-f3ea-4475-ae99-9bf8f983b811; _fbp=fb.1.1714914403398.2105784403; _atrk_siteuid=NFyddQNFN-x4SQ52; rskxRunCookie=0; rCookie=6iqp0pzg7mn39v5dpb4rbhlvtjrcn5; CookieConsent={stamp:%270FEHhZQFbYiJi1L1mL6dpXipTO6DC5JUo/IyYNINn7dy7l4J/JM49Q==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1714914408154%2Cregion:%27tw%27}; CID=5193; UD1=DSA; UD2=prod; _gac_UA-49763723-1=1.1714914608.CjwKCAjw3NyxBhBmEiwAyofDYfULyA4qlKSCHenILVKqSy_TdmEjxWl6Oy-KA1epMczndHtkaD1UZxoCT00QAvD_BwE; _gac_UA-117438867-1=1.1714914608.CjwKCAjw3NyxBhBmEiwAyofDYfULyA4qlKSCHenILVKqSy_TdmEjxWl6Oy-KA1epMczndHtkaD1UZxoCT00QAvD_BwE; _hjSessionUser_628088=eyJpZCI6ImZiMmE1Y2NiLTI5MGItNTI4Yy1hOWVkLTM2MDA0Yjg2YjY0NSIsImNyZWF0ZWQiOjE3MTQ5MTQ0MDM0NzMsImV4aXN0aW5nIjp0cnVlfQ==; appier_utmz=%7B%22csr%22%3A%22(adwords%20gclid)%22%2C%22timestamp%22%3A1714914609%2C%22lcsr%22%3A%22google%22%7D; _gcl_aw=GCL.1714914622.CjwKCAjw3NyxBhBmEiwAyofDYfULyA4qlKSCHenILVKqSy_TdmEjxWl6Oy-KA1epMczndHtkaD1UZxoCT00QAvD_BwE; csrf_cookie_name=369e9272dd4ef3247d84855a78b71ab1; KKWEB=a%3A4%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%2260ce529663f2bc9b6dae3a460c5ee51d%22%3Bs%3A7%3A%22channel%22%3Bs%3A5%3A%22GUEST%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1715095892%3Bs%3A9%3A%22user_data%22%3Bs%3A0%3A%22%22%3B%7Daf01067317432d307ab774c54c0c1770; __lt__sid=a051619c-f20367a4; _hjSession_628088=eyJpZCI6ImU5ZGYxMzYyLWEyODQtNGIxZC05YTIxLTAzZDdlNmE2MWFiYyIsImMiOjE3MTUwOTU4OTQ0ODQsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjowLCJzcCI6MH0=; _gid=GA1.2.263406581.1715095895; lastRskxRun=1715096148311; _ga=GA1.2.363046285.1714914403; _uetsid=e327de400c8611ef841e05dd3e2dad2c; _uetvid=51d425000ae011efb76c03e7a6092020; cto_bundle=cvlyTF91V3BjVU95bVBLdXBwVjVUa2FyN1NES3htNU5hMmdaYVBTUVJjMEJUcTVRQ29KNnZDMjd3TEpsMG9KNFBHSUxWODhEYmtjN3Nsd2UyJTJCQjlNQWNQN3lWTE5jazZNU3U0YXNxMERnJTJCJTJGU2pVdiUyQmZPZW1vVEolMkJQdE42V1FZWkJ5YlRPdSUyQmE4T1hHNlJkREd6SEhkUG9hMndVOUg5eHBWWFhFWEo0UnJNRzE1RGxPSGlCVVk5RWx3R0RDdmcyenNrOWhUUCUyRm1OaVhDSGltMWdWd0hyY0F5cjlWS3VmRkRaSzRkSWglMkJZcElMSlBXZzlxSGdmRE1ocWl5aDhlUnBkVjN2Q0dvVm9JeU01STNnOVZOSVVETWhhTHFqa1U4M21hcFBxNjYySDR4ald6ajVPNG9YSXBBJTJCREF6a2J5TmFrODZMbQ; datadome=l1LtOxHYZUdFB9KJ3vfA1~uFN47eJ3N4qMEjOxquuB_Jd4bzYpHQFBHwVSBdiHHxhWoUj9Ysg_pcELD003qi~TRxQGjRo9slts3l08lF4sFv_sjfhY_C0PT3GTnJtVoX; _ga_RJJY5WQFKP=GS1.1.1715095894.3.1.1715096194.13.0.0'
+    }
 
     dir_path = os.path.join(os.getcwd(), "new_data//camping_region")
     file_name = os.path.join(dir_path, 'camping_kkday.json')
@@ -225,20 +225,18 @@ def kkday_crawler():
     new_obj = 0
     page = 1
     while True:
-        browser = webdriver.Chrome(options=browserOptions)
-        wait = WebDriverWait(browser, 20)
-        browser.get(url.format(page))
-        
-        print(browser.title)
-        print(browser.current_url)
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".product-listview > a > .row")))
-        campings = browser.find_element(By.CSS_SELECTOR, ".product-listview > a > .row")
-        print(len(campings))
+        querystring = {"country":"","city":"","keyword":"%E9%9C%B2%E7%87%9F","availstartdate":"","availenddate":"","cat":"","time": "", "glang":"", "sort": "popularity", "page":page, "row": 10,
+                       "fprice": "*", "eprice": "*", "precurrency": "TWD", "csrf_token_name": "369e9272dd4ef3247d84855a78b71ab1"}
+        res = requests.get(url.format(page), headers=headers)
+        print(res.status_code)
+        print(res)
+
+
         break
     print("==================== End KKday Crawler ====================")
 
 if __name__ == "__main__":
     # asiayo_crawler()
     # easycamp_crawler()
-    klook_crawler()
-    # kkday_crawler()
+    # klook_crawler()
+    kkday_crawler()
