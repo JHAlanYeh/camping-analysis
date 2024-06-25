45a8938ed67007b36528993c36d4de1cf3adee61
DESKTOP-P4V0NDV\Alan <huyij486491@gmail.com>
2024-06-25 11:56:16 +0800

    no message

--- code/gpt35_generator.py
+++ code/gpt35_generator.py
@@ -22,17 +22,17 @@ low_flag = False
 
 df_mid_gan_csv = pd.read_csv("new_data/docs/gpt35_type2_mid_gan_df.csv", encoding="utf-8-sig")
 df_mid_gan_csv[['sequence_num']] = df_mid_gan_csv[['sequence_num']].astype(int)
-# df_low_gan_csv = pd.read_csv("new_data/docs/gpt35_type2_low_gan_df.csv", encoding="utf-8-sig")
-# df_low_gan_csv[['sequence_num']] = df_low_gan_csv[['sequence_num']].astype(int)
-# print(f"增生：負向{len(df_low_gan_csv)}句，中立{len(df_mid_gan_csv)}句")
+df_low_gan_csv = pd.read_csv("new_data/docs/gpt35_type2_low_gan_df.csv", encoding="utf-8-sig")
+df_low_gan_csv[['sequence_num']] = df_low_gan_csv[['sequence_num']].astype(int)
+print(f"增生：負向{len(df_low_gan_csv)}句，中立{len(df_mid_gan_csv)}句")
 
 df_mid_gan = []
 for index, row in list(df_mid_gan_csv.iterrows()):
     df_mid_gan.append(dict(row))
 
 df_low_gan = []
-# for index, row in list(df_low_gan_csv.iterrows()):
-#     df_low_gan.append(dict(row))
+for index, row in list(df_low_gan_csv.iterrows()):
+    df_low_gan.append(dict(row))
 
 client = OpenAI(
   api_key=OPENAI_API_KEY,
@@ -106,7 +106,7 @@ while len(df_mid) + len(df_mid_gan) < target_count:
         print(f"Origin: {row['content']}")
 
         same_sequence_list = list(filter(lambda x: int(x["sequence_num"]) == int(row["sequence_num"]), df_mid_gan))
-        if len(same_sequence_list) >= 23:
+        if len(same_sequence_list) >= 33:
             continue
         same_sequence_data = list(map(lambda x: x["content"], same_sequence_list))
 
