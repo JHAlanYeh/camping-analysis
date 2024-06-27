c0b770f79fc566841b111c401b67987733250cd1
DESKTOP-P4V0NDV\Alan <huyij486491@gmail.com>
2024-06-28 07:22:52 +0800

    no message

--- code/llama3_ckip_generator_traindataset.py
+++ code/llama3_ckip_generator_traindataset.py
@@ -3,8 +3,8 @@ import numpy as np
 import time
 
 
-df = pd.read_csv("new_data/docs/type2_train_df.csv")
-all_df = pd.read_csv("new_data/docs/llama3_type2_merge_df.csv")
+df = pd.read_csv("new_data/docs/type1_train_df.csv")
+all_df = pd.read_csv("new_data/docs/llama3_type1_merge_df.csv")
 
 target_count = len(df[df["label"] == 2])
 print(f"需增生至{target_count}句")
@@ -28,16 +28,14 @@ for index, row in df_low.iterrows():
     print(f"序號：{row['sequence_num']}")
 
     filter_df = all_df[(all_df["sequence_num"] == row["sequence_num"]) & (all_df["origin"] == 0)]
-
+    print(len(filter_df))
 
     low_gan_df = pd.concat([low_gan_df, filter_df]) 
     
 
-    low_gan_df.to_csv('new_data/docs/llama3_type2_low_gan_dataset.csv', index=False, encoding="utf-8-sig")
+    low_gan_df.to_csv('new_data/docs/llama3_type1_low_gan_dataset.csv', index=False, encoding="utf-8-sig")
     print(f"目前增生數量： 增生{len(low_gan_df)}句，總共{len(low_gan_df) + len(df_low)}，目標{target_count}")
 
-
- 
 # # ========================================================= #
 # 中立
 for index, row in df_mid.iterrows():
@@ -54,6 +52,6 @@ for index, row in df_mid.iterrows():
     mid_gan_df = pd.concat([mid_gan_df, filter_df]) 
     
 
-    mid_gan_df.to_csv('new_data/docs/llama3_type2_mid_gan_dataset.csv', index=False, encoding="utf-8-sig")
+    mid_gan_df.to_csv('new_data/docs/llama3_type1_mid_gan_dataset.csv', index=False, encoding="utf-8-sig")
     print(f"目前增生數量： 增生{len(mid_gan_df)}句，總共{len(mid_gan_df) + len(df_mid)}，目標{target_count}")
 
