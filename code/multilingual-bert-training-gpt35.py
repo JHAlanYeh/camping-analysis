5b457ab7e30bcdbe6b6d22fef6e2a917a28ea1d0
DESKTOP-P4V0NDV\Alan <huyij486491@gmail.com>
2024-07-24 23:11:55 +0800

    no message

--- code/multilingual-bert-training-gpt35.py
+++ code/multilingual-bert-training-gpt35.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 import math
+from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer, BertModel, BertConfig
 from transformers import DataCollatorWithPadding
@@ -16,10 +17,10 @@ import matplotlib.pyplot as plt
 import sklearn.metrics as skm
 import seaborn as sns
 
-
-PRETRAINED_MODEL_NAME = "bert-base-multilingual-cased"  # 指定繁簡中文 BERT-BASE 預訓練模型
+PRETRAINED_MODEL_NAME = "bert-base-multilingual-cased"  # 指定繁簡中文 MultilingualBERT-BASE 預訓練模型
 NUM_LABELS = 3
 random_seed = 82
+result_text = ""
 
 # 取得此預訓練模型所使用的 tokenizer
 tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
@@ -47,16 +48,19 @@ class MultilingualBertClassifier(nn.Module):
         super(MultilingualBertClassifier, self).__init__()
         self.model = BertModel.from_pretrained(PRETRAINED_MODEL_NAME)
         self.config = BertConfig.from_pretrained(PRETRAINED_MODEL_NAME)
-        self.dropout = nn.Dropout(0.5)
-        self.linear = nn.Linear(self.config.hidden_size, NUM_LABELS)
-        self.relu = nn.ReLU()
+        self.pre_classifier = nn.Linear(self.config.hidden_size, self.config.hidden_size)        
+        self.dropout = nn.Dropout(0.5)        
+        self.classifier = nn.Linear(self.config.hidden_size, NUM_LABELS)    
 
     def forward(self, input_id, mask):
-        _, pooled_output = self.model(input_ids=input_id, attention_mask=mask, return_dict=False)
-        dropout_output = self.dropout(pooled_output)
-        linear_output = self.linear(dropout_output)
-        final_layer = self.relu(linear_output)
-        return final_layer
+        output_1 = self.model(input_ids=input_id, attention_mask=mask)        
+        hidden_state = output_1[0]        
+        pooler = hidden_state[:, 0]        
+        pooler = self.pre_classifier(pooler)        
+        pooler = torch.nn.ReLU()(pooler)        
+        pooler = self.dropout(pooler)        
+        output = self.classifier(pooler)        
+        return output   
 
 def setup_seed(seed):
     torch.manual_seed(seed)
@@ -67,7 +71,7 @@ def setup_seed(seed):
 
 
 def save_model(model, save_name):
-    torch.save(model.state_dict(), f'new_data/docs/Final_GPT35/Type2_Result/MultilingualBERT/{save_name}')
+    torch.save(model.state_dict(), f'new_data/docs_0724/Final_GPT35/Type1_Result/MultilingualBERT/{save_name}')
 
 def train_model():
     start_time = datetime.now()
@@ -126,11 +130,15 @@ def train_model():
                 total_acc_val += acc
                 total_loss_val += batch_loss.item()
 
-            print(f'''Epochs: {epoch_num + 1}
+            training_info = f'''Epochs: {epoch_num + 1}
             | Train Loss: {total_loss_train / len(train_dataset): .3f}
             | Train Accuracy: {total_acc_train / len(train_dataset): .3f}
             | Val Loss: {total_loss_val / len(dev_dataset): .3f}
-            | Val Accuracy: {total_acc_val / len(dev_dataset): .3f}''')
+            | Val Accuracy: {total_acc_val / len(dev_dataset): .3f}\n'''
+            print(training_info)
+
+            save_result(training_info, "a+")
+            save_result("=====================================\n", "a+")
 
             loss_list.append(total_loss_train / len(train_dataset))
             accuracy_list.append(100 * total_acc_train / len(train_dataset))
@@ -138,10 +146,12 @@ def train_model():
             accuracy_val_list.append(100 * total_acc_val / len(dev_dataset))
 
             # 保存最优的模型
-            print(f"total_acc_val / len(dev_dataset) = {total_acc_val / len(dev_dataset)}, best_dev_acc = {best_dev_acc}")
+            print(f"total_acc_val / len(dev_dataset) = {'%.2f' % (total_acc_val / len(dev_dataset) * 100)}, best_dev_acc = {'%.2f' %  (best_dev_acc * 100)}")
+            save_result(f"total_acc_val / len(dev_dataset) = {'%.2f' %  (total_acc_val / len(dev_dataset) * 100)}, best_dev_acc = {'%.2f' %  (best_dev_acc * 100)}\n", "a+")
             if total_acc_val / len(dev_dataset) > best_dev_acc:
                 best_dev_acc = total_acc_val / len(dev_dataset)
                 save_model(model, 'best.pt')
+                best_epoch = epoch
 
         model.train()
 
@@ -157,13 +167,16 @@ def train_model():
 
     total_time = end_time - start_time
     print(f"Total time:{total_time}")
+    save_result("=====================================\n", "a+")
+    save_result(f"Total time:{total_time}\n", "a+")
+    save_result(f"Best Epoch:{best_epoch}\n", "a+")
 
 
 def evaluate(dataset):
-    # dataset = pd.read_csv("../model/origin_type2/test_df.csv").to_numpy()
+    # dataset = pd.read_csv("../model/origin_type1/test_df.csv").to_numpy()
     # 加载模型
     model = MultilingualBertClassifier()
-    model.load_state_dict(torch.load('new_data/docs/Final_GPT35/Type2_Result/MultilingualBERT/best.pt'))
+    model.load_state_dict(torch.load('new_data/docs_0724/Final_GPT35/Type1_Result/MultilingualBERT/best.pt'))
     model = model.to(device)
     model.eval()
     test_loader = DataLoader(dataset, batch_size=batch_size)
@@ -182,36 +195,42 @@ def evaluate(dataset):
             acc = (output.argmax(dim=1) == test_label).sum().item()
             total_acc_test += acc
     print(f'Test Accuracy: {total_acc_test / len(dataset): .3f}')
-    cf_matrix = confusion_matrix(y_true, y_pred) 
+    cf_matrix = confusion_matrix(y_true, y_pred)
     show_confusion_matrix(y_true, y_pred, 3, "MultilingualBERT", epoch+1)
     print(accuracy_score(y_true, y_pred))
-    # print(classification_report(y_true, y_pred, target_names=['負向', '中立' '正向']))    
+    # print(classification_report(y_true, y_pred, target_names=['負向', '中立' '正向'])) 
     print(cf_matrix)  
-    print("scikit-learn precision:", precision_score(y_true, y_pred, average="weighted"))
-    print("scikit-learn f1 score:", f1_score(y_true, y_pred, average="weighted"))
-    print("scikit-learn recall score:", recall_score(y_true, y_pred, average="weighted"))
     print("scikit-learn Accuracy:", accuracy_score(y_true, y_pred))
+    print("scikit-learn Precision:", precision_score(y_true, y_pred, average="weighted"))
+    print("scikit-learn Recall Score:", recall_score(y_true, y_pred, average="weighted"))
+    print("scikit-learn F1 Score:", f1_score(y_true, y_pred, average="weighted"))
 
+    save_result("=====================================\n", "a+")
+    save_result("scikit-learn Accuracy:" + '%.2f' % (accuracy_score(y_true, y_pred) * 100) + "\n", "a+")
+    save_result("scikit-learn Precision:" + '%.2f' % (precision_score(y_true, y_pred, average="weighted") * 100) + "\n", "a+")
+    save_result("scikit-learn Recall Score:" + '%.2f' % (recall_score(y_true, y_pred, average="weighted") * 100) + "\n", "a+")
+    save_result("scikit-learn F1 Score:" + '%.2f' % (f1_score(y_true, y_pred, average="weighted") * 100) + "\n", "a+")
+    
 
 def draw_loss_image(loss_list, loss_val_list):
     plt.figure()
     plt.plot(loss_list, label = 'train loss')
     plt.plot(loss_val_list, label = 'val loss')
-    plt.title('Multilingual BERT Training and validation loss')
+    plt.title('MultilingualBERT Training and validation loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoches')
     plt.legend()
-    plt.savefig("new_data/docs/Final_GPT35/Type2_Result/MultilingualBERT/MultilingualBERT_Loss.jpg")
+    plt.savefig("new_data/docs_0724/Final_GPT35/Type1_Result/MultilingualBERT/MultilingualBERT_Loss.jpg")
 
 def draw_acc_image(accuracy_list, accuracy_val_list):
     plt.figure()
     plt.plot(accuracy_list, label = 'train acc')
     plt.plot(accuracy_val_list, label = 'val acc')
-    plt.title('Multilingual BERT Training and validation acc')
+    plt.title('MultilingualBERT Training and validation acc')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoches')
     plt.legend()
-    plt.savefig("new_data/docs/Final_GPT35/Type2_Result/MultilingualBERT/MultilingualBERT_Acc.jpg")
+    plt.savefig("new_data/docs_0724/Final_GPT35/Type1_Result/MultilingualBERT/MultilingualBERT_Acc.jpg")
 
 def show_confusion_matrix(y_true, y_pred, class_num, fname, epoch):
     cm = skm.confusion_matrix(y_true, y_pred)
@@ -223,34 +242,39 @@ def show_confusion_matrix(y_true, y_pred, class_num, fname, epoch):
     plt.title(f'{fname} Confusion Matrix', fontsize=15)
     plt.ylabel('Actual label')
     plt.xlabel('Predict label')
-    plt.savefig(fname=f"new_data/docs/Final_GPT35/Type2_Result/MultilingualBERT/{fname}.jpg")
+    plt.savefig(fname=f"new_data/docs_0724/Final_GPT35/Type1_Result/MultilingualBERT/{fname}.jpg")
+
+
+def save_result(text, write_type):
+    file_path = "new_data/docs_0724/Final_GPT35/Type1_Result/MultilingualBERT/result.txt"
+    open(file_path, write_type).close()
+    with open(file_path, write_type) as f:
+        f.write(text)
+        f.close()
+
 
 if __name__ == "__main__":
     print(torch.__version__, torch.cuda.is_available())
     setup_seed(random_seed)
 
-
-    df_train = pd.read_csv("new_data/docs/Final_GPT35/Type2_Result/gpt35_type2_merge_train_dataset.csv")
-    df_val = pd.read_csv("new_data/docs/Final_GPT35/Type2_Result/val_df.csv")
-    df_test = pd.read_csv("new_data/docs/Final_GPT35/Type2_Result/test_df.csv")
-    
-    df_train = shuffle(df_train)
-    df_val = shuffle(df_val)
-    df_test = shuffle(df_test)
+    df_train = pd.read_csv("new_data/docs_0724/Final_GPT35/Type1_Result/gpt35_train_df.csv")
+    df_val = pd.read_csv("new_data/docs_0724/Final_GPT35/Type1_Result/val_df.csv")
+    df_test = pd.read_csv("new_data/docs_0724/Final_GPT35/Type1_Result/test_df.csv")
 
     # 因为要进行分词，此段运行较久，约40s
     train_dataset = MyDataset(df_train, "train")
     dev_dataset = MyDataset(df_val, "train")
     test_dataset = MyDataset(df_test, "test")
 
-
     print(len(df_train), len(dev_dataset), len(test_dataset))
 
-
-    print("Multilingual BERT")
+    print("MultilingualBERT")
     print("=====================================")
 
     # 训练超参数
+    save_result("MultilingualBERT", "w")
+    save_result("\n=====================================\n", "a+")
+    best_epoch = 0
     epoch = 10
     batch_size = 8
     lr = 2e-5
--- code/multilingual-bert-training-llama_3_taiwan.py
+++ code/multilingual-bert-training-llama_3_taiwan.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
 import math
+from pathlib import Path
 from torch.utils.data import Dataset, DataLoader
 from transformers import BertTokenizer, BertModel, BertConfig
-from transformers import AutoTokenizer, AutoModel, AutoConfig
 from transformers import DataCollatorWithPadding
 from torch import nn
 from torch.optim import Adam
@@ -17,9 +17,10 @@ import matplotlib.pyplot as plt
 import sklearn.metrics as skm
 import seaborn as sns
 
-PRETRAINED_MODEL_NAME = "WENGSYX/Deberta-Chinese-Large"
+PRETRAINED_MODEL_NAME = "bert-base-multilingual-cased"  # 指定繁簡中文 MultilingualBERT-BASE 預訓練模型
 NUM_LABELS = 3
-random_seed = 72
+random_seed = 82
+result_text = ""
 
 # 取得此預訓練模型所使用的 tokenizer
 tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)
@@ -42,21 +43,24 @@ class MyDataset(Dataset):
     def __len__(self):
         return len(self.labels)
 
-class AlbertClassfier(nn.Module):
+class MultilingualBertClassifier(nn.Module):
     def __init__(self):
-        super(AlbertClassfier, self).__init__()
-        self.model = AutoModel.from_pretrained(PRETRAINED_MODEL_NAME)
-        self.config = AutoConfig.from_pretrained(PRETRAINED_MODEL_NAME)
-        self.dropout = nn.Dropout(0.5)
-        self.linear = nn.Linear(self.config.hidden_size, NUM_LABELS)
-        self.relu = nn.ReLU()
+        super(MultilingualBertClassifier, self).__init__()
+        self.model = BertModel.from_pretrained(PRETRAINED_MODEL_NAME)
+        self.config = BertConfig.from_pretrained(PRETRAINED_MODEL_NAME)
+        self.pre_classifier = nn.Linear(self.config.hidden_size, self.config.hidden_size)        
+        self.dropout = nn.Dropout(0.5)        
+        self.classifier = nn.Linear(self.config.hidden_size, NUM_LABELS)    
 
-    def forward(self, token_ids):
-        pooled_output=self.model(token_ids)[1] #句向量 [batch_size,hidden_size]
-        dropout_output=self.dropout(pooled_output)
-        linear_output=self.linear(dropout_output)  #[batch_size,num_class]
-        final_layer = self.relu(linear_output)
-        return final_layer
+    def forward(self, input_id, mask):
+        output_1 = self.model(input_ids=input_id, attention_mask=mask)        
+        hidden_state = output_1[0]        
+        pooler = hidden_state[:, 0]        
+        pooler = self.pre_classifier(pooler)        
+        pooler = torch.nn.ReLU()(pooler)        
+        pooler = self.dropout(pooler)        
+        output = self.classifier(pooler)        
+        return output   
 
 def setup_seed(seed):
     torch.manual_seed(seed)
@@ -67,13 +71,13 @@ def setup_seed(seed):
 
 
 def save_model(model, save_name):
-    torch.save(model.state_dict(), f'new_data/docs/Final_Origin/Type2_Result/Deberta/{save_name}')
+    torch.save(model.state_dict(), f'new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/MultilingualBERT/{save_name}')
 
 def train_model():
     start_time = datetime.now()
     print(start_time.strftime("%Y-%m-%d %H:%M:%S"))
     # 定义模型
-    model = AlbertClassfier()
+    model = MultilingualBertClassifier()
     # 定义损失函数和优化器
     criterion = nn.CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=lr)
@@ -96,9 +100,9 @@ def train_model():
         total_loss_train = 0
         for inputs, labels in tqdm(train_loader):
             input_ids = inputs['input_ids'].squeeze(1).to(device) # torch.Size([32, 35])
-            # masks = inputs['attention_mask'].to(device) # torch.Size([32, 1, 35])
+            masks = inputs['attention_mask'].to(device) # torch.Size([32, 1, 35])
             labels = labels.to(device)
-            output = model(input_ids)
+            output = model(input_ids, masks)
 
             batch_loss = criterion(output, labels)
             batch_loss.backward()
@@ -117,20 +121,24 @@ def train_model():
             # 循环获取数据集，并用训练好的模型进行验证
             for inputs, labels in tqdm(dev_loader):
                 input_ids = inputs['input_ids'].squeeze(1).to(device) # torch.Size([32, 35])
-                # masks = inputs['attention_mask'].to(device) # torch.Size([32, 1, 35])
+                masks = inputs['attention_mask'].to(device) # torch.Size([32, 1, 35])
                 labels = labels.to(device)
-                output = model(input_ids)
+                output = model(input_ids, masks)
 
                 batch_loss = criterion(output, labels)
                 acc = (output.argmax(dim=1) == labels).sum().item()
                 total_acc_val += acc
                 total_loss_val += batch_loss.item()
 
-            print(f'''Epochs: {epoch_num + 1}
+            training_info = f'''Epochs: {epoch_num + 1}
             | Train Loss: {total_loss_train / len(train_dataset): .3f}
             | Train Accuracy: {total_acc_train / len(train_dataset): .3f}
             | Val Loss: {total_loss_val / len(dev_dataset): .3f}
-            | Val Accuracy: {total_acc_val / len(dev_dataset): .3f}''')
+            | Val Accuracy: {total_acc_val / len(dev_dataset): .3f}\n'''
+            print(training_info)
+
+            save_result(training_info, "a+")
+            save_result("=====================================\n", "a+")
 
             loss_list.append(total_loss_train / len(train_dataset))
             accuracy_list.append(100 * total_acc_train / len(train_dataset))
@@ -138,10 +146,12 @@ def train_model():
             accuracy_val_list.append(100 * total_acc_val / len(dev_dataset))
 
             # 保存最优的模型
-            print(f"total_acc_val / len(dev_dataset) = {total_acc_val / len(dev_dataset)}, best_dev_acc = {best_dev_acc}")
+            print(f"total_acc_val / len(dev_dataset) = {'%.2f' % (total_acc_val / len(dev_dataset) * 100)}, best_dev_acc = {'%.2f' %  (best_dev_acc * 100)}")
+            save_result(f"total_acc_val / len(dev_dataset) = {'%.2f' %  (total_acc_val / len(dev_dataset) * 100)}, best_dev_acc = {'%.2f' %  (best_dev_acc * 100)}\n", "a+")
             if total_acc_val / len(dev_dataset) > best_dev_acc:
                 best_dev_acc = total_acc_val / len(dev_dataset)
                 save_model(model, 'best.pt')
+                best_epoch = epoch
 
         model.train()
 
@@ -157,13 +167,16 @@ def train_model():
 
     total_time = end_time - start_time
     print(f"Total time:{total_time}")
+    save_result("=====================================\n", "a+")
+    save_result(f"Total time:{total_time}\n", "a+")
+    save_result(f"Best Epoch:{best_epoch}\n", "a+")
 
 
 def evaluate(dataset):
     # dataset = pd.read_csv("../model/origin_type1/test_df.csv").to_numpy()
     # 加载模型
-    model = AlbertClassfier()
-    model.load_state_dict(torch.load('new_data/docs/Final_Origin/Type2_Result/Deberta/best.pt'))
+    model = MultilingualBertClassifier()
+    model.load_state_dict(torch.load('new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/MultilingualBERT/best.pt'))
     model = model.to(device)
     model.eval()
     test_loader = DataLoader(dataset, batch_size=batch_size)
@@ -173,9 +186,9 @@ def evaluate(dataset):
     with torch.no_grad():
         for test_input, test_label in test_loader:
             input_id = test_input['input_ids'].squeeze(1).to(device)
-            # mask = test_input['attention_mask'].to(device)
+            mask = test_input['attention_mask'].to(device)
             test_label = test_label.to(device)
-            output = model(input_id)
+            output = model(input_id, mask)
             _, preds = torch.max(output, 1)       
             y_pred.extend(preds.view(-1).detach().cpu().numpy())       # 將preds預測結果detach出來，並轉成numpy格式       
             y_true.extend(test_label.view(-1).detach().cpu().numpy())   
@@ -183,35 +196,41 @@ def evaluate(dataset):
             total_acc_test += acc
     print(f'Test Accuracy: {total_acc_test / len(dataset): .3f}')
     cf_matrix = confusion_matrix(y_true, y_pred)
-    show_confusion_matrix(y_true, y_pred, 3, "Deberta", epoch+1)
+    show_confusion_matrix(y_true, y_pred, 3, "MultilingualBERT", epoch+1)
     print(accuracy_score(y_true, y_pred))
-    # print(classification_report(y_true, y_pred, target_names=[0, 1, 2]))
+    # print(classification_report(y_true, y_pred, target_names=['負向', '中立' '正向'])) 
     print(cf_matrix)  
-    print("scikit-learn precision:", precision_score(y_true, y_pred, average="weighted"))
-    print("scikit-learn f1 score:", f1_score(y_true, y_pred, average="weighted"))
-    print("scikit-learn recall score:", recall_score(y_true, y_pred, average="weighted"))
     print("scikit-learn Accuracy:", accuracy_score(y_true, y_pred))
+    print("scikit-learn Precision:", precision_score(y_true, y_pred, average="weighted"))
+    print("scikit-learn Recall Score:", recall_score(y_true, y_pred, average="weighted"))
+    print("scikit-learn F1 Score:", f1_score(y_true, y_pred, average="weighted"))
 
+    save_result("=====================================\n", "a+")
+    save_result("scikit-learn Accuracy:" + '%.2f' % (accuracy_score(y_true, y_pred) * 100) + "\n", "a+")
+    save_result("scikit-learn Precision:" + '%.2f' % (precision_score(y_true, y_pred, average="weighted") * 100) + "\n", "a+")
+    save_result("scikit-learn Recall Score:" + '%.2f' % (recall_score(y_true, y_pred, average="weighted") * 100) + "\n", "a+")
+    save_result("scikit-learn F1 Score:" + '%.2f' % (f1_score(y_true, y_pred, average="weighted") * 100) + "\n", "a+")
+    
 
 def draw_loss_image(loss_list, loss_val_list):
     plt.figure()
     plt.plot(loss_list, label = 'train loss')
     plt.plot(loss_val_list, label = 'val loss')
-    plt.title('Deberta Training and validation loss')
+    plt.title('MultilingualBERT Training and validation loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoches')
     plt.legend()
-    plt.savefig("new_data/docs/Final_Origin/Type2_Result/Deberta/Deberta_Loss.jpg")
+    plt.savefig("new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/MultilingualBERT/MultilingualBERT_Loss.jpg")
 
 def draw_acc_image(accuracy_list, accuracy_val_list):
     plt.figure()
     plt.plot(accuracy_list, label = 'train acc')
     plt.plot(accuracy_val_list, label = 'val acc')
-    plt.title('Deberta Training and validation acc')
+    plt.title('MultilingualBERT Training and validation acc')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoches')
     plt.legend()
-    plt.savefig("new_data/docs/Final_Origin/Type2_Result/Deberta/Deberta_Acc.jpg")
+    plt.savefig("new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/MultilingualBERT/MultilingualBERT_Acc.jpg")
 
 def show_confusion_matrix(y_true, y_pred, class_num, fname, epoch):
     cm = skm.confusion_matrix(y_true, y_pred)
@@ -223,33 +242,39 @@ def show_confusion_matrix(y_true, y_pred, class_num, fname, epoch):
     plt.title(f'{fname} Confusion Matrix', fontsize=15)
     plt.ylabel('Actual label')
     plt.xlabel('Predict label')
-    plt.savefig(fname=f"new_data/docs/Final_Origin/Type2_Result/Deberta/{fname}.jpg")
+    plt.savefig(fname=f"new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/MultilingualBERT/{fname}.jpg")
+
+
+def save_result(text, write_type):
+    file_path = "new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/MultilingualBERT/result.txt"
+    open(file_path, write_type).close()
+    with open(file_path, write_type) as f:
+        f.write(text)
+        f.close()
+
 
 if __name__ == "__main__":
     print(torch.__version__, torch.cuda.is_available())
     setup_seed(random_seed)
 
-
-    df_train = pd.read_csv("new_data/docs/Final_Origin/Type2_Result/train_df.csv")
-    df_val = pd.read_csv("new_data/docs/Final_Origin/Type2_Result/val_df.csv")
-    df_test = pd.read_csv("new_data/docs/Final_Origin/Type2_Result/test_df.csv")
-
-    df_train = shuffle(df_train)
-    df_val = shuffle(df_val)
-    df_test = shuffle(df_test)
+    df_train = pd.read_csv("new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/gpt35_train_df.csv")
+    df_val = pd.read_csv("new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/val_df.csv")
+    df_test = pd.read_csv("new_data/docs_0724/Final_Llama3Taiwan/Type1_Result/test_df.csv")
 
     # 因为要进行分词，此段运行较久，约40s
     train_dataset = MyDataset(df_train, "train")
     dev_dataset = MyDataset(df_val, "train")
     test_dataset = MyDataset(df_test, "test")
 
-
     print(len(df_train), len(dev_dataset), len(test_dataset))
 
-    print("Deberta")
+    print("MultilingualBERT")
     print("=====================================")
 
     # 训练超参数
+    save_result("MultilingualBERT", "w")
+    save_result("\n=====================================\n", "a+")
+    best_epoch = 0
     epoch = 10
     batch_size = 8
     lr = 2e-5
